cb2d74ea270fb59e8f68de87eaf28ed93e7920f4
Nikomaru <nikomaru@nikomaru.dev>
2024-10-07 12:41:34 +0900

    front init commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "~/components/ui/button";
+
+
+export const runtime = "edge";
+
+export default function Home() {
+  return (
+    <div>
+        <Button>
+            Hello Panda css
+        </Button>
+    </div>
+  );
+}
